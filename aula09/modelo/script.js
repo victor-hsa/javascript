61330966fc46dec6bc792036db4db40f85e28f82
victor-hsa <vhsa1898@gmail.com>
2025-09-03 23:13:24 -0300

    trabalho

--- aula09/modelo/script.js
+++ aula09/modelo/script.js
@@ -1,1 +1,25 @@
-alert('Olá')
+function carregar(){
+
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+
+//var hora = 12
+
+msg.innerHTML = `Agora são ${hora} horas.`
+
+if (hora >= 0 && hora < 12) {
+    document.body.style.background = '#e2cd9f'
+    img.src = 'manha.png'
+} else if (hora >= 12 && hora <= 18) {
+    document.body.style.background = '#b9846f'
+    img.src = 'tarde.png'
+
+    window.Document.style.img = height='100px'
+    
+} else {
+    document.body.style.background = '#515154'
+    img.src = 'noite.png'
+}
+}

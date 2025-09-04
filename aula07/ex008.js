04afd63841004575365ffd9bdf1c157ab75156dc
victor-hsa <vhsa1898@gmail.com>
2025-09-03 21:07:00 -0300

    atualização

---
+++ aula07/ex008.js
@@ -0,0 +1,7 @@
+var vel = 59
+console.log(`A velocidade do seu caro é de ${vel} km/h`)
+if (vel > 60) { //Condição simples
+    console.log(`Você está a cima da velocidade máxima permitida. MULTADO!`)
+}
+
+console.log('Dirija com cuidado, use sempre o cinto de segurança!')
---
+++ aula07/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Gringo safado! Cai fora')
+}
